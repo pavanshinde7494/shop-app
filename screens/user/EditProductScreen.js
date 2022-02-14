51e08da2d7e051a716cb8349d5fd6816b7f6c7da
pavanshinde7494 <pavanshinde7494@gmail.com>
2022-02-14 21:17:48 +0530

    Input Handling Completed

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,6 +1,6 @@
 import { Ionicons } from '@expo/vector-icons';
 
-import React , {useEffect, useState} from 'react'
+import React , {useEffect, useState,useReducer} from 'react'
 import { View ,
     Text,
     ScrollView,
@@ -11,6 +11,35 @@ import { View ,
 
 import { useSelector , useDispatch } from 'react-redux'
 import { createProduct , updateProduct } from '../../store/actions/products';
+
+
+
+const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE'
+const formReducer = (state , action)=>{
+    if(action.type === FORM_INPUT_UPDATE){
+        const updatedValues = {
+            ...state.inputValues,
+            [action.input] : action.value
+        }
+        const updatedValidities = {
+            ...state.inputValidities,
+            [action.input] : action.isValid
+        }
+        let formIsValid = true;
+        for(const key in updatedValidities) {
+            if(updatedValidities[key] == false){
+                formIsValid = false;
+                break;
+            }
+        }
+        return {
+            inputValues : updatedValues ,
+            inputValidities : updatedValidities,
+            formIsValid : formIsValid
+        }
+    }
+    return state;
+}
 
 export default function EditProductScreen(props) {
 
@@ -27,24 +56,60 @@ export default function EditProductScreen(props) {
         )
     }
 
-    
+    const [formState , dispatchFormState] = useReducer(formReducer , {
+        inputValues : {
+            title : editedProduct ? editedProduct.title : '',
+            imageUrl : editedProduct ? editedProduct.imageUrl : '',
+            price : '',
+            description : editedProduct ? editedProduct.description : '',            
+        },
+        inputValidities : {
+            title : editedProduct ? true : false,
+            imageUrl : editedProduct ? true : false,
+            price : editedProduct ? true : false,
+            description : editedProduct ? true : false,
+        },
+        formIsValid : editedProduct ? true : false 
+    });
+   
 
-
-    const [title , setTitle] = useState(editedProduct ? editedProduct.title : '');
-    const [imageUrl,setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '');
-    const [price,setPrice] = useState('');
-    const [description , setDescription] = useState(editedProduct ? editedProduct.description : '');
-
-
-    
+    const textChangeHandler = (inputIdentifier , text)=>{
+        let isValid = false;
+        if(text.trim().length > 0){
+            isValid = true;
+        }
+        dispatchFormState({
+            type : FORM_INPUT_UPDATE , 
+            value : text , 
+            isValid : isValid,
+            input : inputIdentifier
+        })
+        
+    }
     
 
     const submitHandler = ()=>{
+        if(!formState.formIsValid){
+            Alert.alert('Wrong Input!','Please check error in form.',[
+                {text : 'Okay' }
+            ]);
+            return;
+        }
         if(editedProduct){
-            dispatch(updateProduct(prodId,title,description,imageUrl));
+            dispatch(updateProduct(
+                prodId,
+                formState.inputValues.title,
+                formState.inputValues.description,
+                formState.inputValues.imageUrl)
+            );
         }
         else{
-            dispatch(createProduct(title,description,imageUrl, +price));
+            dispatch(createProduct(
+                formState.inputValues.title,
+                formState.inputValues.description,
+                formState.inputValues.imageUrl, 
+                +formState.inputValues.price)
+            );
         }
         props.navigation.navigate('UserProducts');
     }
@@ -66,21 +131,81 @@ export default function EditProductScreen(props) {
             <View style={styles.form}>
                 <View style={styles.formControl}>
                     <Text style = {styles.label}>Title</Text>
-                    <TextInput style={styles.input} onChangeText={(text)=>setTitle(text)} value={title} />
+                    <TextInput 
+                        style={styles.input} 
+                        onChangeText={
+                            textChangeHandler.bind(this,'title')
+                        } 
+                        value={formState.inputValues.title} 
+                        keyboardType = 'default'
+                        autoCapitalize='sentences'
+                        autoCorrect={true}
+                        returnKeyType='next'
+                        // onEndEditing={()=>{
+                        //     console.log('On ending editing');
+                        // }}
+                        // onSubmitEditing={()=>{
+                        //     console.log('Editing');
+                        // }}
+                    />
+                    {!formState.inputValidities.title && 
+                        <Text
+                            style={{fontFamily : 'open-sans' , fontSize : 10 , color:'red'}}
+                        >Please Enter a Valid title !!
+                        </Text>
+                    }
                 </View>
                 <View style={styles.formControl}>
                     <Text style = {styles.label}>Image URL</Text>
-                    <TextInput style={styles.input} onChangeText={(text)=>setImageUrl(text)} value={imageUrl}/>
+                    <TextInput 
+                        style={styles.input} 
+                        onChangeText={
+                            textChangeHandler.bind(this,'imageUrl')
+                        } 
+                        value={formState.inputValues.imageUrl}
+                    />
                 </View>
+                {!formState.inputValidities.imageUrl && 
+                        <Text
+                            style={{fontFamily : 'open-sans' , fontSize : 10 , color:'red'}}
+                        >Please Enter a Valid Image URL !!
+                        </Text>
+                    }
                 { editedProduct ? null : <View style={styles.formControl}>
-                    <Text style = {styles.label}>Price</Text>
-                    <TextInput style={styles.input} onChangeText={(text)=>setPrice(text)} value={price} />
-                </View>
+                        <Text style = {styles.label}>Price</Text>
+                        <TextInput 
+                            style={styles.input} 
+                            onChangeText={
+                                textChangeHandler.bind(this,'price')
+                            } 
+                            value={formState.inputValues.price} 
+                            keyboardType='decimal-pad'
+                        />
+                    </View>
                 }
+                {!editedProduct && !formState.inputValidities.price && 
+                        <Text
+                            style={{fontFamily : 'open-sans' , fontSize : 10 , color:'red'}}
+                        >Please Enter a Valid Price !!
+                        </Text>
+                    }
                 <View style={styles.formControl}>
                     <Text style = {styles.label}>Description</Text>
-                    <TextInput  style={styles.input} onChangeText={(text)=>setDescription(text)} value={description} />
+                    <TextInput  
+                        style={styles.input} 
+                        onChangeText={
+                            textChangeHandler.bind(this,'description')
+                        } 
+                        value={formState.inputValues.description} 
+                        multiline
+                        />
                 </View>
+                {!formState.inputValidities.description && 
+                    <Text
+                        style={{fontFamily : 'open-sans' , fontSize : 10 , color:'red'}}
+                    >Please Enter a Valid description !!
+                    </Text>
+                }
             </View>
         </ScrollView>
     )
